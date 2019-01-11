484948cc1fc9173359e5ee68a2ea019d83643f23
Micah Walker <minimelon19@gmail.com>
2019-01-11 12:03:38 -0600

    minor fixes

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
     const newScore = this.state.currentScore + 1;
     this.setState({
       currentScore: newScore,
-      rightWrong: ""
+      rightWrong: "Correct!"
     });
     if (newScore >= this.state.topScore) {
       this.setState({ topScore: newScore });
@@ -55,7 +55,7 @@ class App extends Component {
     this.setState({
       currentScore: 0,
       topScore: this.state.topScore,
-      rightWrong: "",
+      rightWrong: "Incorrect!",
       clicked: []
     });
     this.handleShuffle();
