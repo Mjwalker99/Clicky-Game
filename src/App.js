0c27b8abc8e11257b2021742128229c4af3b85b4
Micah Walker <minimelon19@gmail.com>
2019-01-11 12:01:06 -0600

    added nba images

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
     this.setState({
       currentScore: 0,
       topScore: this.state.topScore,
-      rightWrong: "Glaven!",
+      rightWrong: "",
       clicked: []
     });
     this.handleShuffle();
@@ -70,15 +70,14 @@ class App extends Component {
     return (
       <Wrapper>
         <Nav
-          title="Simpsons Clicky Game"
+          title="NBA Clicky Game"
           score={this.state.currentScore}
           topScore={this.state.topScore}
           rightWrong={this.state.rightWrong}
         />
 
         <Title>
-          Try to click on each character, but don't hit any duplicates, or
-          we'll release the hounds!!!
+       NBA Memory Game. Don't Click The Same Player Twice!
         </Title>
 
         <Container>
